fcebca33c99d4940c6d8b82a898b7b94e64a3d6a
pkreyenb <philipp.kreyenberg@iup.uni-heidelberg.de>
2017-12-07 22:38:42 +0100

    Moved the warping words to seperate file to edit in Carlos_video_synthesizer.py.

--- Carlo_warping_words.py
+++ Carlo_warping_words.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec  7 21:07:26 2017
+Created on Thu Dec  7 21:16:23 2017
 
 @author: pkreyenb
 """
 
 import pyaudio
-import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import aubio as ab
 
-import pygame as pg
-pg.font.init() # you have to call this at the start, 
+import pygame
+pygame.font.init() # you have to call this at the start, 
                    # if you want to use this module.
 import random
 
@@ -23,8 +22,6 @@ import queue
 import time
 
 import argparse
-
-item_switch = "drifting_words" # "wobbling_circle", "drifting_words"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-input", required=False, type=int, help="Audio Input Device")
@@ -39,57 +36,47 @@ if not args.input:
     print("Run this program with -input 1, or the number of the input you'd like to use.")
     exit()
 
-pg.init()
+pygame.init()
 
 if args.f:
     screenWidth, screenHeight = 1024, 768
-    screen = pg.display.set_mode((screenWidth, screenHeight), pg.FULLSCREEN | pg.HWSURFACE | pg.DOUBLEBUF)
+    screen = pygame.display.set_mode((screenWidth, screenHeight), pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF)
 
 else:
     screenWidth, screenHeight = 600, 600
-    screen = pg.display.set_mode((screenWidth, screenHeight))
+    screen = pygame.display.set_mode((screenWidth, screenHeight))
 
 white = (255, 255, 255)
 black = (0, 0, 0)
-
-
-
-class Circle(object):
-    def __init__(self, x, y, color, size, tone_energy, wobble_scaling, lifetime):
+        
+class TextSurface(object):
+    def __init__(self, x, y, color, size, text):
         self.x = x
         self.y = y
         self.color = color
         self.size = size
-        self.energy = tone_energy
-        self.wobble_scaling = wobble_scaling
-        self.lifetime = lifetime
-
-    def draw(self, surface):
-        pg.draw.circle(surface, self.color, (self.x, self.y), self.size)
-
-    def wobble(self, surface, wobble_size):
-        self.lifetime -= 1
-        self.size = self.size + np.int(self.energy*self.wobble_scaling*wobble_size)
-#        self.size += self.lifetime*random.randint(np.int(-self.energy*self.wobble_scaling),
-#                                                  np.int(self.energy*self.wobble_scaling))
-        self.draw(surface)
+        self.text = text
+        self.myfont = pygame.font.SysFont('Padauk', self.size)
+        self.textsurface = self.myfont.render(self.text, False, self.color)
+        self.lifetime = 100
         
+    def move(self, increment_x, increment_y):
+        self.lifetime -= 1
+        self.x +=increment_x
+        self.y +=increment_y
         
 
 def drawColorFromCmap(rand_nummer, colormap):
     rgb_tuple = colormap(rand_nummer)
     return (np.int(rgb_tuple[0]*255), np.int(rgb_tuple[1]*255), np.int(rgb_tuple[2]*255))   
 
-
-colors = [(229, 244, 227), (93, 169, 233), (0, 63, 145), (255, 255, 255), (109, 50, 109)]
 itemList = []
 words = ["Frieder Carlo", "Dr. Dr. Hyper!"]
-wobble_sizes = [1,0,-1,0]
 
 # initialise pyaudio
 p = pyaudio.PyAudio()
 
-clock = pg.time.Clock()
+clock = pygame.time.Clock()
 
 # open stream
 
@@ -120,40 +107,42 @@ sound_features = [] #init empty list to later contain note, freq and energy
 
 q = queue.Queue()
 
-item = Circle(screenWidth//2, screenHeight//2,
-              random.choice(colors), 100, 0, 1e4, 10)
-
 def draw_pygame():
     running = True
     note_counter = 0
     while running:
-        key = pg.key.get_pressed()
+        key = pygame.key.get_pressed()
 
-        if key[pg.K_q]:
+        if key[pygame.K_q]:
             running = False
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 running = False
 
         if not q.empty():
             note_counter +=1
             b = q.get()
-            print(b)
-            item.energy = b[2]
-            screen.fill(black)
-            for wobble_size in wobble_sizes:
-                item.wobble(screen, wobble_size)
-                print(wobble_size)
-                #print(item.size)
+            newItem = TextSurface(random.randint(0, screenWidth), random.randint(0, screenHeight),
+                                  drawColorFromCmap(random.randint(0, 255), plt.cm.rainbow),
+                                  random.randint(20, 200), random.choice(words))
+#           newCircle = TextSurface(screenWidth/6, screenHeight/2,
+#                                   drawColorFromCmap(random.randint(0, 255), plt.cm.jet),
+#                                   100, random.choice(words))
+            itemList.append(newItem)
+        for place, item in enumerate(itemList):
+            if item.lifetime < 1:
+                itemList.pop(place)
+            else:
+                screen.blit(item.textsurface,(item.x, item.y))
                 
-        
+            item.move(-1, -1)
                 
-        pg.display.flip()
-        clock.tick(90)
+        pygame.display.flip()
+        clock.tick(10)
         
 
-    if note_counter > 20:
-        note_counter = 0
+        if note_counter > 20:
+            note_counter = 0
 
 def get_onsets():
     while True:
@@ -162,16 +151,8 @@ def get_onsets():
             audiobuffer = stream.read(buffer_size, exception_on_overflow=False)
             signal = np.fromstring(audiobuffer, dtype=np.float32)
 
-            pitch_midi = pitch_o(signal)[0]
-            pitch_note = ab.midi2note(np.int(pitch_midi))
-            pitch_freq = ab.miditofreq(pitch_midi)
-            energy = np.sum(signal**2)/len(signal)
-#            print(pitch_note)
-            sound_features = [pitch_note, pitch_freq, energy]
             if onset(signal):
-                q.put(sound_features)
-#            if energy > 1e-4:
-#                print(energy*1e4)
+                q.put(True)
 
         except KeyboardInterrupt:
             print("*** Ctrl+C pressed, exiting")
@@ -185,4 +166,4 @@ t.start()
 draw_pygame()
 stream.stop_stream()
 stream.close()
-pg.display.quit()
+pygame.display.quit()
